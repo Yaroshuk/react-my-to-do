e7b31ee9cc1116bcbd8ef7a0efdd21d215d071ff
Sergey Yaroshuk <yaroshukmail@gmail.com>
2018-05-30 15:33:41 +0300

    Fix api request

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getWeather = ({ cityId, APPID }) => {
-  return axios.get('http://api.openweathermap.org/data/2.5/weather', {
+  return axios.get('https://api.openweathermap.org/data/2.5/weather', {
     params: {
       units: 'metric',
       id: cityId,
